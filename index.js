992bba465f21efbec10c27d23d1e74d85471c1af
Walker, Corey <corey_walker@mcafee.com>
2020-03-16 21:45:44 -0700

    create user questions and setup api calls

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
 
+const questions = [
+    {
+        message: "Enter your GitHub username.",
+        name: "username"
+    },
+    {
+        message: "Enter the title of your project.",
+        name: "title"
+    },
+    {
+        message: "Enter a description for your project.",
+        name: "desc"
+    },
+    {
+        message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+        name: "install"
+    },
+    {
+        message: "Provide instructions and examples for use.",
+        name: "usage"
+    },
+    {
+        message: "Choose a license that works best for your project.",
+        name: "license",
+        choices: [
+            "Apache License 2.0",
+            "GNU GPLv3",
+            "MIT",
+            "ISC"
+        ]
+    },
+    {
+        message: "List your collaborators, if any.",
+        name: "credits"
+    },
+    {
+        message: "Provide examples on how to run your test",
+        name: "tests"
+    }
 ];
 
 function writeToFile(fileName, data) {
+    fs.writeFile("repos.txt", repoNamesStr, function(err) {
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success!");
+    }) ;
 }
 
 function init() {
--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,15 @@
+const axios = require("axios");
+
 const api = {
   getUser(username) {
-
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    axios.get(queryUrl)
+    .then(function(res){
+      console.log(res.data);
+      // const repoNames = [];
+      // res.data.forEach(repo => repoNames.push(repo.name));
+      // const repoNamesStr = repoNames.join("\n");
+    });
   }
 };
 
